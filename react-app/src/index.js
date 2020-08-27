a6654f28921eb905863a8643d24af9ac7b49bba4
chuanyuan <liuchuanyuan@gmail.com>
2020-08-27 19:21:33 +1000

    Added routing for development

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./LoginPage/App";
+import HomePage from "./HomePage/HomePage";
+import * as serviceWorker from "./serviceWorker";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Router>
+            <ul>
+                <li>
+                    <Link to='/'>Home Page</Link>
+                </li>
+                <li>
+                    <Link to='/login'>Login Page</Link>
+                </li>
+            </ul>
+
+            <Route exact path='/' component={HomePage} />
+            <Route path='/login' component={App} />
+        </Router>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
