a0240a5e71b548c3ab7f071b4b323ae385ba1502
Zijian-Ju <53360473+Zijian-Ju@users.noreply.github.com>
2020-09-01 23:55:43 +1000

    evaluation listing page and search bar

---
+++ react-app/src/EvaluationPage/evaluationData.js
@@ -0,0 +1,19 @@
+const evaluationData = [
+{
+    id:1,
+    author: "Gerald",
+    framework: "Primary EdTect Framework",
+    date: "11/5/2020",
+    status: "active"
+}
+,
+{
+    id:2,
+    author: "Gale",
+    framework: "VR EdTech Framework",
+    date: "11/5/2020",
+    status: "completed"
+}
+
+]
+export default evaluationData
