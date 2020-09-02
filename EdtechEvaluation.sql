4a8b31c17b1454ad94c1cee3ab3305e5df47ffce
Anu <anuradhag@unimelb.edu.au>
2020-09-02 21:59:38 +1000

    populated database tables

--- EdtechEvaluation.sql
+++ EdtechEvaluation.sql
@@ -15,10 +15,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-DROP DATABASE IF EXISTS edtech_evaluation;
-CREATE DATABASE edtech_evaluation;
-USE edtech_evaluation;
-
 --
 -- Table structure for table `evaluation`
 --
