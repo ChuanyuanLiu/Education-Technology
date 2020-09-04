cc4007934cd0c00c30be86a90a5fe01fff7af20a
Ma-Yuyao <antyaoyao@gmail.com>
2020-09-04 18:02:32 +1000

    1st attempt

--- node-app/server.js
+++ node-app/server.js
@@ -6,6 +6,16 @@ var reportRouter = require('./routes/report');
 
 var app = express();
 
+//Set up cross-domain access 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 // Delegate routers for request data types
 app.use('/framework', frameworkRouter);
 app.use('/evaluation', evaluationRouter);
