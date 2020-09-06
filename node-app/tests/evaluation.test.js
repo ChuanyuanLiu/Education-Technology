2b77666d1fe6e4504d88b34fec1650fd846bd1ec
Samuel Tumewa <stumewa@yahoo.com>
2020-09-06 21:15:10 +1000

    Commented uncomplete sections

--- node-app/tests/evaluation.test.js
+++ node-app/tests/evaluation.test.js
@@ -109,67 +109,67 @@ describe("POST /evaluation/update/response?evaluation_id={eid}&question_id={qid}
 
 });
 
-describe("POST /update/title", () => {
+// describe("POST /update/title", () => {
 
-    test("Should update evaluation title and summary for evaluation_id=1", done => {
+//     test("Should update evaluation title and summary for evaluation_id=1", done => {
     
-        var title = "St. Arthur Evalutaion";
-        var summary = "Requires a second audit";
-        var id = 1; 
+//         var title = "St. Arthur Evalutaion";
+//         var summary = "Requires a second audit";
+//         var id = 1; 
         
-    const input = "UPDATE evaluation "
-        + "SET evaluation_title = '" + title
-        + "', evaluation_summary = '" + summary
-        + "' WHERE evaluation_id = " + id;
+//     const input = "UPDATE evaluation "
+//         + "SET evaluation_title = '" + title
+//         + "', evaluation_summary = '" + summary
+//         + "' WHERE evaluation_id = " + id;
 
-         sqlConnector.sqlCall(input, function(res) 
-        {
-            expect(res).toEqual(expect.anything());
-            done();
-        });
-    });
-});
+//          sqlConnector.sqlCall(input, function(res) 
+//         {
+//             expect(res).toEqual(expect.anything());
+//             done();
+//         });
+//     });
+// });
 
-describe('the evaluation_id', () => {
-    test('is 1', () => {
+// describe('the evaluation_id', () => {
+//     test('is 1', () => {
     
-        var title = "St. Arthur Evalutaion";
-        var summary = "Requires a second audit";
-        var id = 1; 
+//         var title = "St. Arthur Evalutaion";
+//         var summary = "Requires a second audit";
+//         var id = 1; 
         
-    const input = "UPDATE evaluation "
-        + "SET evaluation_title = '" + title
-        + "', evaluation_summary = '" + summary
-        + "' WHERE evaluation_id = " + id;
+//     const input = "UPDATE evaluation "
+//         + "SET evaluation_title = '" + title
+//         + "', evaluation_summary = '" + summary
+//         + "' WHERE evaluation_id = " + id;
 
-         sqlConnector.sqlCall(input, function(res) 
-        {
-                expect(res.id).toBe(1);
-                expect(res.title).toBe('"St. Arthur Evalutaion"');
-                expect(res.summary).toBe('"Requires a second audit"');
-                done();    
-            });
-        });
-    });
+//          sqlConnector.sqlCall(input, function(res) 
+//         {
+//                 expect(res.id).toBe(1);
+//                 expect(res.title).toBe('"St. Arthur Evalutaion"');
+//                 expect(res.summary).toBe('"Requires a second audit"');
+//                 done();    
+//             });
+//         });
+//     });
 
- afterAll(() => {
-    sqlConnector.closeConnection();
-});
+//  afterAll(() => {
+//     sqlConnector.closeConnection();
+// });
 
-const evaluation1 = {
-    evaluation_title: '"St. Arthur Evalutaion"',
-    evaluation_id: 1,
-  };
-  const evaluation2 = {
-    evaluation_title: '"St. Arthur Evalutaion"',
-    evaluation_id: 1,
-  };
+// const evaluation1 = {
+//     evaluation_title: '"St. Arthur Evalutaion"',
+//     evaluation_id: 1,
+//   };
+//   const evaluation2 = {
+//     evaluation_title: '"St. Arthur Evalutaion"',
+//     evaluation_id: 1,
+//   };
   
-  describe('the evaluation with id 1 and 2', () => {
-    test('have all the same values', () => {
-      expect(evaluation1).toEqual(evaluation2);
-    });
-    test('are not the exact same can', () => {
-      expect(evaluation1).not.toBe(evaluation2);
-    });
-  });
+//   describe('the evaluation with id 1 and 2', () => {
+//     test('have all the same values', () => {
+//       expect(evaluation1).toEqual(evaluation2);
+//     });
+//     test('are not the exact same can', () => {
+//       expect(evaluation1).not.toBe(evaluation2);
+//     });
+//   });
