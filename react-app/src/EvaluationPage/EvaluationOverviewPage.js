c58f3a8d2e6f63f149b936eef7c9702ce3dbbb03
chuanyuan <liuchuanyuan@gmail.com>
2020-09-06 16:39:43 +1000

    evaluation overview page intergrated with backend

--- react-app/src/EvaluationPage/EvaluationOverviewPage.js
+++ react-app/src/EvaluationPage/EvaluationOverviewPage.js
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import NavBar from "../Utils/NavBar";
 import "./EvaluationOverview.css";
 import framework_data from "./framework.json";
-import { useHistory } from "react-router-dom";
+import {useHistory} from "react-router-dom";
 /*
 Evaluation Page Layout
 |-- NavBar
@@ -12,9 +12,9 @@ Evaluation Page Layout
                 |-- Question
 */
 
-function Summary({data = ""}) {
+function Summary({summary = ""}) {
     // track changes to summary
-    const [getSummary, setSummary] = useState(data);
+    const [getSummary, setSummary] = useState(summary);
     const appendSummary = (event) => {
         setSummary(event.target.value);
     };
@@ -44,34 +44,45 @@ function Summary({data = ""}) {
 
 // data is a question object that contains
 // Question_Title, Question_ID
-function Question({question_title, question_id, section_index, index}) {
+function Question({section_id, question_id, question_title}) {
     const history = useHistory();
-    function handleClick(){
-        history.push('./question')
+    function handleClick() {
+        history.push("./question");
     }
-    return <li onClick={handleClick}>{`${section_index+1}.${index+1} ${question_title}`}</li>;
+    return (
+        <li onClick={handleClick}>
+        {`${section_id}.${question_id} ${question_title}`}</li>
+    );
 }
 
 // data is a list of question objects each contains
 // Question_Title, Question_ID
-function Section( {section_title, section_id, questions, index}) {
+function Section({section_title, section_id, questions}) {
     // track expand or not
     const [getExpand, setExpand] = useState(false);
     const toggleExpand = (event) => {
         event.preventDefault();
         setExpand(!getExpand);
-    }
+    };
 
     return (
         <>
-            <div className="sub_header">
-                {`Section ${index + 1} ${section_title}`}
-                <button className="right" onClick={toggleExpand}>{getExpand ? "collapse" : "expand"}</button>
+            <div className='sub_header'>
+                {`Section ${section_id} ${section_title}`}
+                <button className='right' onClick={toggleExpand}>
+                    {getExpand ? "collapse" : "expand"}
+                </button>
             </div>
             <ul>
-                {getExpand ? questions.map((s, i) => (
-                    <Question  {...s} key={i} index={i} section_index={index} />
-                )) : null}
+                {getExpand
+                    ? questions.map((question, i) => (
+                          <Question
+                              {...question}
+                              section_id = {section_id}
+                              key={i}
+                          />
+                      ))
+                    : null}
             </ul>
         </>
     );
@@ -83,19 +94,51 @@ function SectionsList({sections}) {
     return (
         <>
             <div className='section_header'>Sections</div>
-            {sections.map((s, i) => (
-                <Section key={i} index={i} {...s} />
+            {sections.map((section, i) => (
+                <Section key={i} {...section} />
             ))}
         </>
     );
 }
 
-function EvaluationOverviewPage(props) {
+function EvaluationOverviewPage({history}) {
+    // Data is stored in history.location that is controlled by useHistory Hook
+    // TODO: data does not persist after refreshing of page, currently default to 1
+    const {evaluation_id = 1, framework_id = 1} = history.location;
+    const [evaluation_data, setEvaluation] = useState(null);
+
+    // fetch data every time evaluation or framework ID changes
+    useEffect(() => {
+        fetch(
+            `http://localhost:3001/evaluation?evaluation_id=${evaluation_id}&framework_id=${framework_id}`
+        )
+            .then(response => response.json())
+            .then(setEvaluation)
+            .catch(console.error);
+    }, [evaluation_id, framework_id]);
+
+    if (evaluation_id == null || framework_id == null) {
+        return (
+            <div className='EvaluationPage'>
+                <NavBar title={"Invalid Evaluation"} />
+                <h1>
+                    This page does not exist, please try accessing another
+                    evaluation
+                </h1>
+            </div>
+        );
+    }
+
+    if (evaluation_data == null) {
+        return <h1>Loading...</h1>;
+    }
+
+    console.log(evaluation_data);
     return (
         <div className='EvaluationPage'>
-            <NavBar title={framework_data.framework_title} />
-            <Summary />
-            <SectionsList {...framework_data}  />
+            <NavBar title={evaluation_data.evaluation_title} />
+            <Summary summary={evaluation_data.evaluation_summary}/>
+            <SectionsList {...evaluation_data} />
         </div>
     );
 }
