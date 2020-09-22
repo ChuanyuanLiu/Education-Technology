112860f831ac3f4f39e7c42e393a897f30919328
Anu <anuradhag@unimelb.edu.au>
2020-09-22 11:22:26 +1000

    update question added from Yao

--- node-app/routes/framework.js
+++ node-app/routes/framework.js
@@ -206,4 +206,32 @@ router.post('/update', function(req, res, next){
 }
 });
 
+//Update question 
+router.post('/section/question/rate/update', function (req, res, next) {
+    // Example: http://localhost:3001/framework/section/question/rate/update?question_id={qid}
+    if (req.query.question_id != null) {
+        var rate_1_criterion = req.body.rate_1_criterion;
+        var rate_2_criterion = req.body.rate_2_criterion;
+        var rate_3_criterion = req.body.rate_3_criterion;
+        var rate_4_criterion = req.body.rate_4_criterion;
+        var rate_5_criterion = req.body.rate_5_criterion;
+        var question_id = req.query.question_id;
+        console.log(rate_1_criterion);
+        const sql = "UPDATE framework_section_question "
+            + "SET rate_1_criterion = '" + rate_1_criterion
+            + "', rate_2_criterion = '" + rate_2_criterion
+            + "', rate_3_criterion = '" + rate_3_criterion
+            + "', rate_4_criterion = '" + rate_4_criterion
+            + "', rate_5_criterion = '" + rate_5_criterion
+            + "' WHERE question_id = " + question_id;
+        sqlAdapter.sqlCall(sql, function (updateQuestion) {
+            if (updateQuestion == null) {
+                res.send(unsuccessful);
+                return;
+            }
+            res.send(successful);
+        });
+    }
+});
+
 module.exports = router;
