583a21da525a16033b5b1c467bdbabd544225e74
chuanyuan <liuchuanyuan@gmail.com>
2020-09-22 21:13:35 +1000

    tested TextArea

--- react-app/src/FrameworkPage/FrameworkQuestion.test.js
+++ react-app/src/FrameworkPage/FrameworkQuestion.test.js
@@ -1,15 +1,42 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import {render, cleanup, waitForDomChange} from "@testing-library/react";
 import FrameworkQuestionPage from "./FrameworkQuestionPage";
+import data from "./frameworkQuestion.json"
 
 /** Acceptance test 2.2.8
  * Gerald updates the question's rating explanations with appropriate text
  * Assumes this is a new question, and only must be 5 questions
- *  */ 
+ *  */
 
-describe("AC 2.2.8" ,()=>{
-    test("Initiate blank evaluation page", ()=>{
-        return 1;
+// Set DOM to empty after each clean up
+afterEach(cleanup);
+
+// Mock fetch by redefining it
+global.fetch = jest.fn(()=>Promise.resolve(({
+    json: () => Promise.resolve(data)
+})));
+
+describe("AC 2.2.8", () => {
+    test("5 text areas", async () => {
+        const history = {location: {state: {question_id: 1}}};
+        const {getByLabelText} = render(
+            <FrameworkQuestionPage history={history} />
+        );
+        const labels = [
+            "Not Applicable",
+            "Below Basic",
+            "Basic",
+            "Adequate",
+            "Exceptional",
+        ];
+
+        // Wait to loading
+        await waitForDomChange(() => expect(fetch).toHaveBeenCalledTimes(1));
+
+        for (const label_text of labels) {
+            const label = getByLabelText(label_text);
+            expect(label).toBeTruthy();
+        }
     });
 });
 
