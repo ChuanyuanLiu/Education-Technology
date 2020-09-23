ac745bae51b4f03bbb3e4e64d8390f799426c9eb
Samuel Tumewa <stumewa@yahoo.com>
2020-09-24 09:43:52 +1000

    Added tests for updating section title

--- node-app/tests/framework.test.js
+++ node-app/tests/framework.test.js
@@ -73,3 +73,73 @@ describe("POST /framework/update?framework_id={fid}", () => {
     });
 
 });
+
+describe("POST /framework/section/update?section_id={sid}", () => {
+
+    const section_title = "Test Section";
+    const section_title_empty = "";
+    const section_id = 1;
+    const section_id_empty = 0;
+    let prev_title = "";
+
+    const input = `UPDATE framework_section`
+        + ` SET section_title = '${section_title}'`
+        + ` WHERE section_id = ${section_id};`;
+    
+    const inputEmptyTitle = `UPDATE framework_section`
+        + ` SET section_title = '${section_title_empty}'`
+        + ` WHERE section_id = ${section_id};`;
+    
+    const inputInvalidId = `UPDATE framework_section`
+        + ` SET section_title = '${section_title}'`
+        + ` WHERE section_id = ${section_id_empty};`;
+
+    const inputCheck = `SELECT section_title `
+        + `FROM framework_section `
+        + `WHERE section_id = ${section_id};`;
+    
+    const inputCheckAll = `SELECT section_title `
+        + `FROM framework_section;`;
+
+    // Obtain the previous title
+    beforeEach(done => {
+        sqlAdapter.sqlCall(inputCheck, function(prevRes) {
+            prev_title = prevRes[0].section_title;
+            done();
+        });
+    });
+
+    // Reset the title to the previous title after each test
+    afterEach(done => {
+        const resetTable = `UPDATE framework_section`
+            + ` SET section_title = '${prev_title}'`
+            + ` WHERE section_id = ${section_id};`
+        sqlAdapter.sqlCall(resetTable, function(resetRes) {
+            done();
+        });
+    });
+
+    test("Should correctly store the updated section title", done => {
+        sqlAdapter.sqlCall(input + inputCheck, function(inputRes) {
+            expect(inputRes[1][0].section_title).toEqual(section_title);
+            done();
+        });
+    });
+
+    test("Should correctly store an empty section title", done => {
+        sqlAdapter.sqlCall(inputEmptyTitle + inputCheck, function(inputRes) {
+            expect(inputRes[1][0].section_title).toEqual(section_title_empty);
+            done();
+        });
+    });
+
+    test("Should not update any of the titles for an invalid section ID", done => {
+        sqlAdapter.sqlCall(inputInvalidId + inputCheckAll, function(inputRes) {
+            for (let i = 0; i < inputRes[1].length; i++) {
+                expect(inputRes[1][i].section_title).not.toEqual(section_title);
+            }
+            done();
+        }); 
+    });
+
+});
