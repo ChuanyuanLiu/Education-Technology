6a5e40979227d9ba35f32abaf7f04f58641f5f44
Samuel Tumewa <stumewa@yahoo.com>
2020-09-24 08:45:03 +1000

    Refactored evaluation.test.js with new class and database names

---
+++ node-app/tests/framework.test.js
@@ -0,0 +1,16 @@
+// var sqlAdapter = require('../utils/sqlAdapter');
+
+// afterAll(() => {
+//     sqlAdapter.closeConnection();
+// });
+
+// describe("POST /framework/update?framework_id={fid}", () => {
+
+//     const title = "Test Title";
+//     const input = "UPDATE framework"
+//         + " SET framework_title = " + title
+//         + " WHERE framework_id = 1";
+
+    
+
+// });
