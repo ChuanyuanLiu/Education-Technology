c3ada8ab65987b7180931bfd2145872512b97601
Samuel Tumewa <stumewa@yahoo.com>
2020-09-24 15:37:29 +1000

    Added tests for updating question criteria

--- node-app/tests/framework.test.js
+++ node-app/tests/framework.test.js
@@ -22,12 +22,12 @@ describe("POST /framework/update?framework_id={fid}", () => {
         + ` SET framework_title = '${framework_title}'`
         + ` WHERE framework_id = 0;`;
 
-    const inputCheck = `SELECT framework_title `
-        + `FROM framework `
-        + `WHERE framework_id = ${framework_id};`;
+    const inputCheck = `SELECT framework_title`
+        + ` FROM framework`
+        + ` WHERE framework_id = ${framework_id};`;
     
-    const inputCheckAll = `SELECT framework_title `
-        + `FROM framework;`;
+    const inputCheckAll = `SELECT framework_title`
+        + ` FROM framework;`;
 
     // Obtain the previous title
     beforeEach(done => {
@@ -90,12 +90,12 @@ describe("POST /framework/section/update?section_id={sid}", () => {
         + ` SET section_title = '${section_title}'`
         + ` WHERE section_id = 0;`;
 
-    const inputCheck = `SELECT section_title `
-        + `FROM framework_section `
-        + `WHERE section_id = ${section_id};`;
+    const inputCheck = `SELECT section_title`
+        + ` FROM framework_section`
+        + ` WHERE section_id = ${section_id};`;
     
-    const inputCheckAll = `SELECT section_title `
-        + `FROM framework_section;`;
+    const inputCheckAll = `SELECT section_title`
+        + ` FROM framework_section;`;
 
     // Obtain the previous title
     beforeEach(done => {
@@ -201,6 +201,111 @@ describe("POST /framework/section/question/update?question_id={qid}", () => {
         sqlAdapter.sqlCall(inputInvalidId + inputCheckAll, function(inputRes) {
             for (let i = 0; i < inputRes[1].length; i++) {
                 expect(inputRes[1][i].question_title).not.toEqual(question_title);
+            }
+            done();
+        }); 
+    });
+
+});
+
+describe("POST /framework/section/question/rate/update?question_id={qid}", () => {
+
+    const rate_1_criterion = "Test Rate 1";
+    const rate_2_criterion = "Test Rate 2";
+    const rate_3_criterion = "Test Rate 3";
+    const rate_4_criterion = "Test Rate 4";
+    const rate_5_criterion = "Test Rate 5";
+    const question_id = 1;
+    let prev_criteria = [];
+
+    const input = `UPDATE framework_section_question`
+        + ` SET rate_1_criterion = '${rate_1_criterion}',`
+            + ` rate_2_criterion = '${rate_2_criterion}',`
+            + ` rate_3_criterion = '${rate_3_criterion}',`
+            + ` rate_4_criterion = '${rate_4_criterion}',`
+            + ` rate_5_criterion = '${rate_5_criterion}'`
+        + ` WHERE question_id = ${question_id};`;
+    
+    const inputEmptyCriteria = `UPDATE framework_section_question`
+        + ` SET rate_1_criterion = "",`
+            + ` rate_2_criterion = "",`
+            + ` rate_3_criterion = "",`
+            + ` rate_4_criterion = "",`
+            + ` rate_5_criterion = ""`
+    + ` WHERE question_id = ${question_id};`;
+    
+    const inputInvalidId = `UPDATE framework_section_question`
+    + ` SET rate_1_criterion = '${rate_1_criterion}',`
+        + ` rate_2_criterion = '${rate_2_criterion}',`
+        + ` rate_3_criterion = '${rate_3_criterion}',`
+        + ` rate_4_criterion = '${rate_4_criterion}',`
+        + ` rate_5_criterion = '${rate_5_criterion}'`
+    + ` WHERE question_id = 0;`;
+
+    const inputCheck = `SELECT *`
+        + ` FROM framework_section_question`
+        + ` WHERE question_id = ${question_id};`;
+    
+    const inputCheckAll = `SELECT *`
+        + ` FROM framework_section_question;`;
+
+    // Obtain the previous title
+    beforeEach(done => {
+        sqlAdapter.sqlCall(inputCheck, function(prevRes) {
+            prev_criteria.push(prevRes[0].rate_1_criterion);
+            prev_criteria.push(prevRes[0].rate_2_criterion);
+            prev_criteria.push(prevRes[0].rate_3_criterion);
+            prev_criteria.push(prevRes[0].rate_4_criterion);
+            prev_criteria.push(prevRes[0].rate_5_criterion);
+            done();
+        });
+    });
+
+    // Reset the title to the previous title after each test
+    afterEach(done => {
+        const resetTable = `UPDATE framework_section_question`
+            + ` SET rate_1_criterion = '${prev_criteria[0]}',`
+                + ` rate_2_criterion = '${prev_criteria[1]}',`
+                + ` rate_3_criterion = '${prev_criteria[2]}',`
+                + ` rate_4_criterion = '${prev_criteria[3]}',`
+                + ` rate_5_criterion = '${prev_criteria[4]}'`
+            + ` WHERE question_id = ${question_id};`
+        sqlAdapter.sqlCall(resetTable, function(resetRes) {
+            prev_criteria = [];
+            done();
+        });
+    });
+
+    test("Should correctly store the updated criteria", done => {
+        sqlAdapter.sqlCall(input + inputCheck, function(inputRes) {
+            expect(inputRes[1][0].rate_1_criterion).toEqual(rate_1_criterion);
+            expect(inputRes[1][0].rate_2_criterion).toEqual(rate_2_criterion);
+            expect(inputRes[1][0].rate_3_criterion).toEqual(rate_3_criterion);
+            expect(inputRes[1][0].rate_4_criterion).toEqual(rate_4_criterion);
+            expect(inputRes[1][0].rate_5_criterion).toEqual(rate_5_criterion);
+            done();
+        });
+    });
+
+    test("Should correctly store empty criteria", done => {
+        sqlAdapter.sqlCall(inputEmptyCriteria + inputCheck, function(inputRes) {
+            expect(inputRes[1][0].rate_1_criterion).toEqual("");
+            expect(inputRes[1][0].rate_2_criterion).toEqual("");
+            expect(inputRes[1][0].rate_3_criterion).toEqual("");
+            expect(inputRes[1][0].rate_4_criterion).toEqual("");
+            expect(inputRes[1][0].rate_5_criterion).toEqual("");
+            done();
+        });
+    });
+
+    test("Should not update any of the criteria for an invalid question ID", done => {
+        sqlAdapter.sqlCall(inputInvalidId + inputCheckAll, function(inputRes) {
+            for (let i = 0; i < inputRes[1].length; i++) {
+                expect(inputRes[1][i].rate_1_criterion).not.toEqual(rate_1_criterion);
+                expect(inputRes[1][i].rate_2_criterion).not.toEqual(rate_2_criterion);
+                expect(inputRes[1][i].rate_3_criterion).not.toEqual(rate_3_criterion);
+                expect(inputRes[1][i].rate_4_criterion).not.toEqual(rate_4_criterion);
+                expect(inputRes[1][i].rate_5_criterion).not.toEqual(rate_5_criterion);
             }
             done();
         }); 
