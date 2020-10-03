1f5be66d9c9b0b394f45fe2a9ce396539214883d
chuanyuan <liuchuanyuan@gmail.com>
2020-10-03 19:42:03 +1000

    Tests changed published to finalized

--- react-app/src/FrameworkPage/FrameworkOverview.js
+++ react-app/src/FrameworkPage/FrameworkOverview.js
@@ -41,7 +41,6 @@ function FrameworkOverview({history}) {
     const [expandedSections, setExpandedSections] = useState([]);
 
     function initializeFramework(data) {
-        console.log(data);
         setFrameworkTitle(data.framework_title);
         setActiveStatus(data.framework_active_status);
         setSections(data.sections);
