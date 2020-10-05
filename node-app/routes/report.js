afc0e98fa089518697da646d17b6c9c854f77d56
Ma-Yuyao <antyaoyao@gmail.com>
2020-10-06 01:51:29 +1100

    Finished report listing logic. Added 1 sample report in database to test

--- node-app/routes/report.js
+++ node-app/routes/report.js
@@ -2,11 +2,40 @@ var express = require('express');
 var router = express.Router();
 
 var sqlAdapter = require('../utils/sqlAdapter');
+const { response } = require('express');
+const UNSUCCESSFUL = "The call to the SQL database was unsuccessful.";
+const SUCCESSFUL = "The call to the SQL database was successful."
 
 router.get('/', function (req, res, next) {
-    sqlAdapter.sqlCall("SELECT * FROM report", function (sqlRes) {
-        res.send(sqlRes);
-    });
+    if(req.query.report_id != null)
+    {
+        // Example: http://localhost:3001/report?report_id=1
+        // Returns all details about a report
+        const sql = "SELECT * "
+            + "FROM report "
+            + "WHERE report_id = " + req.query.report_id;
+
+        sqlAdapter.sqlCall(sql, function (reportRes) {
+            if (reportRes == null || JSON.stringify(reportRes) == '[]') {
+                res.send(UNSUCCESSFUL);
+                return;
+            }
+            res.send(reportRes);
+        });
+
+    }
+    // Default; return all frameworks
+    else
+    {
+        const sql = "SELECT * FROM report";
+        sqlAdapter.sqlCall(sql, function (sqlRes) {
+            if (sqlRes == null || JSON.stringify(sqlRes) == '[]') {
+                res.send(UNSUCCESSFUL);
+                return;
+            }
+            res.send(sqlRes);
+        });
+    }
 });
 
 module.exports = router;
