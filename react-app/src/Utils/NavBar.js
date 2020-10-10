5a8337100351831e2df49d8f61d3e636e8032a60
Samuel Tumewa <stumewa@yahoo.com>
2020-10-10 22:18:34 +1100

    Root directory redirects to migrated login page

--- react-app/src/Utils/NavBar.js
+++ react-app/src/Utils/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {useHistory} from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 /**
  * A navigation bar with a go back button 
@@ -7,12 +8,18 @@ import {useHistory} from "react-router-dom";
  */
 function NavBar({children}) {
     const history = useHistory();
+    const { logout } = useAuth0();
     return (
         <div className='NavBar'>
             <span className="left clickable" onClick={()=>history.goBack()}>
             {"<"}
             </span>
             {children}
+            <span className="right clickable" onClick={()=>logout({
+                returnTo: window.location.origin
+            })}>
+            {"Log out"}
+            </span>
         </div>
     );
 }
