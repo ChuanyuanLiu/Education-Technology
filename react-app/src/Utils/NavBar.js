f3a088f7719062f35690e71b641234d246e4ab3f
Samuel Tumewa <stumewa@yahoo.com>
2020-10-10 22:41:13 +1100

    Prevent back button from returning to login page

--- react-app/src/Utils/NavBar.js
+++ react-app/src/Utils/NavBar.js
@@ -9,9 +9,16 @@ import { useAuth0 } from "@auth0/auth0-react";
 function NavBar({children}) {
     const history = useHistory();
     const { logout } = useAuth0();
+    console.log(history.length);
     return (
         <div className='NavBar'>
-            <span className="left clickable" onClick={()=>history.goBack()}>
+            <span className="left clickable" onClick={()=> {
+                if (window.location.pathname === "/home_page") {
+                    history.block("Logout to return to login page.");
+                } else {
+                    history.goBack();
+                }
+            }}>
             {"<"}
             </span>
             {children}
