6debd888ebfb69c39d35af961a75e4134e8f994c
chuanyuan <liuchuanyuan@gmail.com>
2020-10-25 18:57:15 +1100

    removed sample files

---
+++ react-app/cypress/integration/examples/U2-Manage Evaluation Framework.spec.js
@@ -0,0 +1,7 @@
+/*
+As Gerald (Senior Consultant), I should be able to create a new framework so 
+that I can change my evaluation to suit different types of EdTech products.
+*/
+it('U2', ()=>{
+    cy.visit('http://localhost:3000/');
+})
