c91347c4a5914634d39e813f129822fcb182a8bc
Samuel Tumewa <stumewa@yahoo.com>
2020-10-31 01:41:32 +1100

    Removed console logs

--- node-app/routes/user.js
+++ node-app/routes/user.js
@@ -48,17 +48,15 @@ router.get('/', function (req, res, next) {
  * @param {any} res - res
  */
 function recurseRoles(index, users, res) {
-    console.log(index + " " + users.length);
+    // console.log(index + " " + users.length);
     if (index < users.length) {
         const url = `https://edtechevaluation.au.auth0.com/api/v2/users/${users[index].user_id}/roles`;
 
         auth0Adapter.auth0Call("GET", url, {}, function (auth0Res) {
-            // console.log(auth0Res);
             users[index].role = JSON.parse(auth0Res)[0];
             recurseRoles(index + 1, users, res);
         });
     } else {
-        console.log(users);
         res.send(users);
     }
 }
@@ -106,7 +104,6 @@ router.post('/update', function (req, res, next) {
     if (req.query.user_id != null) {
         const url = `https://edtechevaluation.au.auth0.com/api/v2/users/${req.query.user_id}`;
         auth0Adapter.auth0Call("PATCH", url, req.body, function (auth0Res) {
-            console.log(auth0Res);
             res.send(auth0Res);
         });
     }
@@ -120,17 +117,14 @@ router.post('/update', function (req, res, next) {
  * @param {any} req.query.user_id - user_id
  */
 router.post('/update/role', function (req, res, next) {
-    console.log("update role");
     if (req.query.user_id != null) {
         const url = `https://edtechevaluation.au.auth0.com/api/v2/users/${req.query.user_id}/roles`;
         const delBody = {"roles": [req.body.old_role_id]};
         const addBody = {"roles": [req.body.new_role_id]};
         // Delete old role
         auth0Adapter.auth0Call("DELETE", url, delBody, function (auth0DelRes) {
-            console.log(auth0DelRes);
             // Add new role
             auth0Adapter.auth0Call("POST", url, addBody, function (auth0AddRes) {
-                console.log(auth0AddRes);
                 res.send(auth0AddRes);
             });
         });
@@ -154,7 +148,6 @@ router.post('/new', function (req, res, next) {
 
     // Create a new user
     auth0Adapter.auth0Call("POST", url, body, function (auth0UserRes) {
-        console.log(auth0UserRes);
         let userRes = JSON.parse(auth0UserRes);
 
         const assignUrl = `https://edtechevaluation.au.auth0.com/api/v2/users/${userRes.user_id}/roles`;
@@ -162,7 +155,6 @@ router.post('/new', function (req, res, next) {
 
         // Assign role to new user
         auth0Adapter.auth0Call("POST", assignUrl, assignBody, function (auth0AssignRes) {
-            console.log(auth0AssignRes);
             res.send(userRes);
         })
     })
@@ -181,7 +173,6 @@ router.get('/delete', function (req, res, next) {
         const url = `https://edtechevaluation.au.auth0.com/api/v2/users/${req.query.user_id}`;
 
         auth0Adapter.auth0Call("DELETE", url, {}, function (auth0Res) {
-            console.log(auth0Res);
             res.send(auth0Res);
         });
     }
